5789620d65c5158216099f41283059c819833bf4
Arpit Sachan <sachanarpitknp@gmail.com>
2021-09-30 00:14:45 +0530

    user patch api writeen and change in product page

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -8,6 +8,13 @@ router.post("", async (req, res) => {
   return res.status(201).send(user);
 });
 
+router.patch("/:id", async (req, res) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  return res.status(200).send({ user });
+});
+
 router.get("", async (req, res) => {
   const user = await User.find().lean().exec();
   return res.status(200).send({ user });
