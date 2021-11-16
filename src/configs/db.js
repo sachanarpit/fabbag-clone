4fdcef3dd47bce9779d02a78814d2d77d1a2e4e8
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-16 22:20:51 +0530

    dot env installed

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+
 const connect = () => {
   return mongoose.connect(
     "mongodb+srv://{username}:{Password}9@cluster0.swzgm.mongodb.net/fabbag_db?retryWrites=true&w=majority"
